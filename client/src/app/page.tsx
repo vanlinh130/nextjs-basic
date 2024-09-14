ed016d735794b7b4e8a05c120c8509b5df13df6c
Văn Linh Lê <linhvanle130@gmail.com>
2024-09-15 01:36:50 +0700

    6.  CSS style trong Next.js

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,15 @@
+import Card from "@/app/Card/Card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center gap-3 px-10">
-      <Button className="px-6" variant='outline'>Văn Linh</Button>
+      <Button className="px-6 text-mini" variant='outline'>Văn Linh</Button>
       <Input  type="email" placeholder="Email"   />
+      <h1>dfdf</h1>
+
+      <Card />
     </div>
   );
 }
