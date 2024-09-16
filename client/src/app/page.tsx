ee455e6d016bf416f27d90d1f0ed32ad03bb1ea8
Văn Linh Lê <linhvanle130@gmail.com>
2024-09-16 15:17:45 +0700

    Metadata, Script

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,11 @@
 
-export default function Home() {
-  return (
-    <main className="flex justify-center gap-4 mt-5">
-      <video width="500" height="240" controls preload="none" loop >
-        <source src="/videos/flower.webm" type="video/mp4"  />
-      </video>
+import type { Metadata } from 'next'
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/gfU1iZnjRZM"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-      
-      <iframe width="1280" height="720" src="https://www.youtube.com/embed/4u856utdR94" frameBorder="0" allowFullScreen></iframe>
-
-    </main>
-  );
+ 
+export const metadata: Metadata = {
+  title: '...',
+  description: '...',
 }
+ 
+export default function Page() {}
+
