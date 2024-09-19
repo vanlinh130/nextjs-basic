4bf53ea1c9ece6217f42bb7b2aa34f38b6980195
Văn Linh Lê <linhvanle130@gmail.com>
2024-09-20 00:53:07 +0700

    15. Hướng dẫn chuyển Dark Light Mode với Shadcn UI Next.js

---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div>
+        <ModeToggle/>
+        <h1>Register Page</h1>
+    </div>
+  )
+}
